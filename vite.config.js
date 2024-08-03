2b0c810a54dbd868cd484e9603744e8a7385bc6e
Ahmed Esmail <97197954+AhmedEsmail8@users.noreply.github.com>
2024-08-03 21:04:17 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/start-framework-website/',
+  base: '/start-react-website/',
   plugins: [react()],
 });
